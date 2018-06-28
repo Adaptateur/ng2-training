829757c9bf77e0a4d768aaa97a9a55d493d9c15f
Adaptateur <Adaptateur@users.noreply.github.com>
2018-06-28 16:05:56 +0200

    Création serveur http node

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,13 +1,25 @@
 const http = require('http');
-const port = 3000;
+const fs = require('fs');
+const port = 8000;
 
 const server = http.createServer(
     (req, resp) => {
-        console.log(req.url);
+      console.log(req.url);
+      if (req.url === '/hello') {
         resp.end('Hello Node.js Server!')
-        }
+      }else if (req.url === '/data'){
+        fs.readFile('../data/plates.json', 'utf-8', (err, data) => {
+          if (err) throw  err;
+          console.log(data);
+          resp.end(data);
+        });
+
+      }else{
+        resp.end('Nothing to do here')
+      }
+    }
         );
-        
+
 server.listen(port, (err) => {
     console.log(`server listening on ${port}`)
 });
